63a902e80e74741118ab510fc73a6a1cbbfc3d34
Adam Ralph <adam@adamralph.com>
2015-10-28 13:59:43 -0500

    publish application events

--- src/HashBus.Application/HashtagTweetedHandler.cs
+++ src/HashBus.Application/HashtagTweetedHandler.cs
@@ -1,17 +1,47 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using HashBus.Application.Events;
 using HashBus.Twitter.Events;
+using LiteGuard;
 using NServiceBus;
+using ColoredConsole;
 
 namespace HashBus.Application
 {
-
     public class HashtagTweetedHandler : IHandleMessages<HashtagTweeted>
     {
-        public Task Handle(HashtagTweeted message)
+        private ISendOnlyBus bus;
+
+        public HashtagTweetedHandler(ISendOnlyBus bus)
         {
-            Console.WriteLine(@"Handling: HashtagTweeted with Id: {0}", message.Id);
-            return Task.FromResult(0);
+            Guard.AgainstNullArgument("bus", bus);
+
+            this.bus = bus;
+        }
+
+        public async Task Handle(HashtagTweeted message)
+        {
+            foreach (var mentionMessage in message.UserMentions.Select(userMention => new UserMentionedWithHashtag
+            {
+                Hashtag = message.Hashtag,
+                IsRetweet = message.IsRetweet,
+                TweetId = message.Id,
+                UserMentionId = userMention.Id,
+                UserMentionIdStr = userMention.IdStr,
+                UserMentionName = userMention.Name,
+                UserMentionScreenName = userMention.ScreenName,
+                TweetCreatedAt = message.CreatedAt,
+            }))
+            {
+                ColorConsole.WriteLine(
+                    $"{mentionMessage.TweetCreatedAt} ".DarkCyan(),
+                    $"#{message.Hashtag} ".Cyan(),
+                    mentionMessage.IsRetweet ? "retweet mentioning ".DarkGreen() : "tweet mentioning ".Green(),
+                    $"{mentionMessage.UserMentionName} ".Yellow(),
+                    $"@{mentionMessage.UserMentionScreenName}".DarkYellow());
+
+                await bus.PublishAsync(mentionMessage);
+            }
         }
     }
 }
