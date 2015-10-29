2eb5e3ff80d057931a4052c5fc5e4e59792610df
Adam Ralph <adam@adamralph.com>
2015-10-29 11:48:20 +0100

    add HashBus.Projection.UserLeaderboard

---
+++ src/HashBus.Projection.UserLeaderboard/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace HashBus.Projection.UserLeaderboard
+{
+    class Program
+    {
+        static void Main()
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("HashBus.Projection.UserLeaderboard");
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.EnableInstallers();
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+            busConfiguration.SendFailedMessagesTo("error");
+            busConfiguration.LimitMessageProcessingConcurrencyTo(1);
+            busConfiguration.RegisterComponents(c =>
+                c.RegisterSingleton<IRepository<string, IEnumerable<LeaderboardProjection.Mention>>>(
+                    new FileListRepository<LeaderboardProjection.Mention>(@"C:\HashBus\LeaderboardProjection.Mention")));
+
+            using (await Bus.Create(busConfiguration).StartAsync())
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
