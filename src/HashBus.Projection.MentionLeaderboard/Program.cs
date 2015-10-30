64cc50fb5a8c1071c4a58576b7e3bd53dcd999eb
sfarmar <s.farmar@gmail.com>
2015-10-30 13:09:06 +0000

    Fixed based on Adam's comments

--- src/HashBus.Projection.MentionLeaderboard/Program.cs
+++ src/HashBus.Projection.MentionLeaderboard/Program.cs
@@ -5,10 +5,13 @@ using NServiceBus;
 
 namespace HashBus.Projection.UserLeaderboard
 {
+    using System;
     using System.Configuration;
 
     class Program
     {
+        const string DataFolderPath = "DATA_FOLDER_PATH";
+
         static void Main()
         {
             AsyncMain().GetAwaiter().GetResult();
@@ -16,9 +19,9 @@ namespace HashBus.Projection.UserLeaderboard
 
         static async Task AsyncMain()
         {
-            if (ConfigurationManager.AppSettings["JSON_FOLDER_PATH"] == null)
+            if (ConfigurationManager.AppSettings[DataFolderPath] == null)
             {
-                throw new ArgumentException("Please make sure you have the 'JSON_FOLDER_PATH' set in your appSettings");
+                throw new ArgumentException("Please make sure you have the 'DATA_FOLDER_PATH' set in your appSettings");
             }
 
             var busConfiguration = new BusConfiguration();
@@ -30,7 +33,7 @@ namespace HashBus.Projection.UserLeaderboard
             busConfiguration.LimitMessageProcessingConcurrencyTo(1);
             busConfiguration.RegisterComponents(c =>
                 c.RegisterSingleton<IRepository<string, IEnumerable<LeaderboardProjection.Mention>>>(
-                    new FileListRepository<LeaderboardProjection.Mention>(ConfigurationManager.AppSettings["JSON_FOLDER_PATH"])));
+                    new FileListRepository<LeaderboardProjection.Mention>(ConfigurationManager.AppSettings[DataFolderPath])));
 
             using (await Bus.Create(busConfiguration).StartAsync())
             {
