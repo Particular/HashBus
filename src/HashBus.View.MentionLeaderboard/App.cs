06730fd260af9a496fd18d764e0532fc5f9e2977
Adam Ralph <adam@adamralph.com>
2015-11-02 19:27:49 +0100

    added option to show percentages against leaderboard entries

--- src/HashBus.View.MentionLeaderboard/App.cs
+++ src/HashBus.View.MentionLeaderboard/App.cs
@@ -8,14 +8,15 @@ namespace HashBus.View.MentionLeaderboard
     class App
     {
         public static async Task RunAsync(
-            string mongoConnectionString, string mongoDBDatabase, string hashtag, int refreshInterval)
+            string mongoConnectionString, string mongoDBDatabase, string hashtag, int refreshInterval, bool showPercentages)
         {
             var mongoDatabase = new MongoClient(mongoConnectionString).GetDatabase(mongoDBDatabase);
 
             await View.StartAsync(
                 hashtag,
                 refreshInterval,
-                new MongoDBListRepository<Mention>(mongoDatabase, "mention_leaderboard__mentions"));
+                new MongoDBListRepository<Mention>(mongoDatabase, "mention_leaderboard__mentions"),
+                showPercentages);
         }
     }
 }
