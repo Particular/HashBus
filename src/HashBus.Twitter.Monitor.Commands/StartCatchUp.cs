a17e8a43338de253015e43cb0df49ae78b9c8df8
sfarmar <s.farmar@gmail.com>
2015-11-06 17:17:20 +0000

    Adding Tweeter integration to catchup service

--- src/HashBus.Twitter.Monitor.Commands/StartCatchUp.cs
+++ src/HashBus.Twitter.Monitor.Commands/StartCatchUp.cs
@@ -1,7 +1,12 @@
 ﻿namespace HashBus.Twitter.Monitor.Commands
 {
+    using System;
+
     public class StartCatchUp
     {
         public long TweetId { get; set; }
+        public string Track { get; set; }
+        public Guid SessionId { get; set; }
+        public string EndpointName { get; set; }
     }
 }
