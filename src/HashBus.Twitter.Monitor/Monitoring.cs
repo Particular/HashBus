5a51085b0bc604ddf213f2a8a328f0bdd4d0ceb5
Adam Ralph <adam@adamralph.com>
2015-11-06 10:48:21 +0100

    added stream restart on failure

--- src/HashBus.Twitter.Monitor/Monitoring.cs
+++ src/HashBus.Twitter.Monitor/Monitoring.cs
@@ -2,6 +2,7 @@ namespace HashBus.Twitter.Monitor
 {
     using System;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     using ColoredConsole;
     using HashBus.Twitter.Events;
@@ -18,63 +19,83 @@ namespace HashBus.Twitter.Monitor
             string consumerSecret,
             string accessToken,
             string accessTokenSecret,
-            Guid sessionId,
             string endpointName)
         {
             var credentials = new TwitterCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
-            var stream = Stream.CreateFilteredStream(credentials);
-
-            stream.AddTrack(track);
-
-            stream.StreamStarted += (sender, args) => ColorConsole.WriteLine($" {track} ".DarkCyan().OnWhite(), " stream started.".Gray());
-            stream.StreamStopped += (sender, args) => ColorConsole.WriteLine($" {track} ".DarkCyan().OnWhite(), " stream stopped.".Red(), $" {args.Exception.Message}".DarkRed());
-
-            stream.MatchingTweetReceived += (sender, e) =>
+            while (true)
             {
-                var message = new TweetReceived
+                try
                 {
-                    EndpointName = endpointName,
-                    SessionId = sessionId,
-                    Track = track,
-                    TweetId = e.Tweet.Id,
-                    TweetCreatedAt = e.Tweet.CreatedAt,
-                    TweetCreatedById = e.Tweet.CreatedBy.Id,
-                    TweetCreatedByIdStr = e.Tweet.CreatedBy.IdStr,
-                    TweetCreatedByName = e.Tweet.CreatedBy.Name,
-                    TweetCreatedByScreenName = e.Tweet.CreatedBy.ScreenName,
-                    TweetIsRetweet = e.Tweet.IsRetweet,
-                    TweetText = e.Tweet.Text,
-                    TweetUserMentions = e.Tweet.UserMentions
-                        .Select(userMention => new UserMention
+                    var stream = Stream.CreateFilteredStream(credentials);
+                    stream.AddTrack(track);
+
+                    var sessionId = Guid.NewGuid();
+                    stream.StreamStarted += (sender, args) =>
+                    {
+                        sessionId = Guid.NewGuid();
+                        ColorConsole.WriteLine(
+                            $"{DateTime.UtcNow.ToLocalTime()}".DarkGray(),
+                            " ",
+                            $" {track} ".DarkCyan().OnWhite(),
+                            " ",
+                            "stream started with session ID".Gray(),
+                            " ",
+                            $"{sessionId}".White());
+                    };
+
+                    stream.StreamStopped += (sender, args) => ColorConsole.WriteLine($"{DateTime.UtcNow.ToLocalTime()} ".DarkGray(), $" {track} ".DarkCyan().OnWhite(), " stream stopped.".Red(), $" {args.Exception.Message}".DarkRed());
+
+                    stream.MatchingTweetReceived += (sender, e) =>
+                    {
+                        var message = new TweetReceived
                         {
-                            Id = userMention.Id,
-                            IdStr = userMention.IdStr,
-                            Indices = userMention.Indices,
-                            Name = userMention.Name,
-                            ScreenName = userMention.ScreenName,
-                        })
-                        .ToArray(),
-                    RetweetedTweetId = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.Id : default(long),
-                    RetweetedTweetCreatedAt = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedAt : default(DateTime),
-                    RetweetedTweetCreatedById = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.Id : default(long),
-                    RetweetedTweetCreatedByIdStr = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.IdStr : default(string),
-                    RetweetedTweetCreatedByName = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.Name : default(string),
-                    RetweetedTweetCreatedByScreenName = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.ScreenName : default(string),
-                    TweetHashtags = e.Tweet.Hashtags
-                        .Select(ht => new Hashtag
-                        {
-                            Text = ht.Text,
-                            Indices = ht.Indices,
-                        }).ToArray(),
-                };
+                            EndpointName = endpointName,
+                            SessionId = sessionId,
+                            Track = track,
+                            TweetId = e.Tweet.Id,
+                            TweetCreatedAt = e.Tweet.CreatedAt,
+                            TweetCreatedById = e.Tweet.CreatedBy.Id,
+                            TweetCreatedByIdStr = e.Tweet.CreatedBy.IdStr,
+                            TweetCreatedByName = e.Tweet.CreatedBy.Name,
+                            TweetCreatedByScreenName = e.Tweet.CreatedBy.ScreenName,
+                            TweetIsRetweet = e.Tweet.IsRetweet,
+                            TweetText = e.Tweet.Text,
+                            TweetUserMentions = e.Tweet.UserMentions
+                                .Select(userMention => new UserMention
+                                {
+                                    Id = userMention.Id,
+                                    IdStr = userMention.IdStr,
+                                    Indices = userMention.Indices,
+                                    Name = userMention.Name,
+                                    ScreenName = userMention.ScreenName,
+                                })
+                                .ToArray(),
+                            RetweetedTweetId = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.Id : default(long),
+                            RetweetedTweetCreatedAt = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedAt : default(DateTime),
+                            RetweetedTweetCreatedById = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.Id : default(long),
+                            RetweetedTweetCreatedByIdStr = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.IdStr : default(string),
+                            RetweetedTweetCreatedByName = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.Name : default(string),
+                            RetweetedTweetCreatedByScreenName = e.Tweet.IsRetweet ? e.Tweet.RetweetedTweet.CreatedBy.ScreenName : default(string),
+                            TweetHashtags = e.Tweet.Hashtags
+                                .Select(ht => new Hashtag
+                                {
+                                    Text = ht.Text,
+                                    Indices = ht.Indices,
+                                }).ToArray(),
+                        };
 
-                Writer.Write(message);
-                bus.Publish(message);
-            };
+                        Writer.Write(message);
+                        bus.Publish(message);
+                    };
 
-            await stream.StartStreamMatchingAnyConditionAsync();
-
-            Console.ReadKey();
+                    await stream.StartStreamMatchingAnyConditionAsync();
+                }
+                catch (Exception ex)
+                {
+                    ColorConsole.WriteLine($"{DateTime.UtcNow.ToLocalTime()} ".DarkGray(), "Error listening to Twitter stream.".Red(), $" {ex.Message}".DarkRed());
+                    Thread.Sleep(1000);
+                }
+            }
         }
     }
 }
