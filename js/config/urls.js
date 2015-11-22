d0b91a65a6beee7149da1f867436596721d00425
Adam Ralph <adam@adamralph.com>
2015-11-22 22:51:08 +0100

    switch from BuildStuffLT to BuildStuffUA

--- js/config/urls.js
+++ js/config/urls.js
@@ -1,11 +1,11 @@
 angular.module('hashBusUI')
-	.factory('urls', function urls(){	
+	.factory('urls', function urls(){
 		return{
-				topTweetersRetweeters:'http://hashbus-demo.cloudapp.net:8080/top-tweeters-retweeters/해시BuildStuffLT',
-				topTweeters:'http://hashbus-demo.cloudapp.net:8080/top-tweeters/해시BuildStuffLT',
-				topRetweeters:'http://hashbus-demo.cloudapp.net:8080/top-retweeters/해시BuildStuffLT',
-				mostMentioned:'http://hashbus-demo.cloudapp.net:8080/most-mentioned/해시BuildStuffLT',
-				mostRetweeted:'http://hashbus-demo.cloudapp.net:8080/most-retweeted/해시BuildStuffLT',
-				mostHashTagged:'http://hashbus-demo.cloudapp.net:8080/most-hashtagged/해시BuildStuffLT',
+				topTweetersRetweeters:'http://hashbus-demo.cloudapp.net:8080/top-tweeters-retweeters/해시BuildStuffUA',
+				topTweeters:'http://hashbus-demo.cloudapp.net:8080/top-tweeters/해시BuildStuffUA',
+				topRetweeters:'http://hashbus-demo.cloudapp.net:8080/top-retweeters/해시BuildStuffUA',
+				mostMentioned:'http://hashbus-demo.cloudapp.net:8080/most-mentioned/해시BuildStuffUA',
+				mostRetweeted:'http://hashbus-demo.cloudapp.net:8080/most-retweeted/해시BuildStuffUA',
+				mostHashTagged:'http://hashbus-demo.cloudapp.net:8080/most-hashtagged/해시BuildStuffUA',
 			}
 	});
