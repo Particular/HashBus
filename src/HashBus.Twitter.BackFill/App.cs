4f31c79c1454bd15c4a0cf78f2708a21d55f6278
Adam Ralph <adam@adamralph.com>
2015-11-30 15:56:20 -0600

    add HashBus.Twitter.BackFill.csproj

---
+++ src/HashBus.Twitter.BackFill/App.cs
@@ -0,0 +1,38 @@
+﻿namespace HashBus.Twitter.BackFill
+{
+    using System;
+    using HashBus.NServiceBusConfiguration;
+    using HashBus.Twitter.Monitor.Commands;
+    using NServiceBus;
+    using NServiceBus.Persistence;
+
+    class App
+    {
+        public static void Run(
+            string nserviceBusConnectionString,
+            string endpointName,
+            string track,
+            long tweetId)
+        {
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName(endpointName);
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.EnableInstallers();
+            busConfiguration.UsePersistence<NHibernatePersistence>().ConnectionString(nserviceBusConnectionString);
+            busConfiguration.ApplyMessageConventions();
+
+            using (var bus = Bus.Create(busConfiguration).Start())
+            {
+                var command = new StartCatchUp
+                {
+                    EndpointName = endpointName,
+                    SessionId = Guid.NewGuid(),
+                    Track = track,
+                    TweetId = tweetId,
+                };
+
+                bus.Send(command);
+            }
+        }
+    }
+}
