bee974a54554a9b41e561152265bde38fd6f8249
Adam Ralph <adam@adamralph.com>
2015-12-02 11:35:32 -0600

    change default hashtag to NSBCon

--- js/controllers/leaderboardsCtrl.js
+++ js/controllers/leaderboardsCtrl.js
@@ -1,7 +1,7 @@
 angular.module('hashBusUI')
 	.controller('leaderboardsCtrl', ['$rootScope', '$scope', '$http', '$stateParams', 'urls', 'poller', '$interval',
 		function leaderboardsCtrl($rootScope, $scope, $http, $stateParams, urls, poller, $interval) {
-            var hashtag = $stateParams.hashtag || "BuildStuffUA";
+            var hashtag = $stateParams.hashtag || "NSBCon";
             $rootScope.title = hashtag;
 			var interval = 10000;
             function map(idProperty, count, previousResults, newResults){
