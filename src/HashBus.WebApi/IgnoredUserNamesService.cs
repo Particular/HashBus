d52efda8c6e8c5b181b9a8695a58b193c5bc5d2d
Adam Ralph <adam@adamralph.com>
2017-10-03 13:23:19 +0200

    remove redundant usings

--- src/HashBus.WebApi/IgnoredUserNamesService.cs
+++ src/HashBus.WebApi/IgnoredUserNamesService.cs
@@ -1,10 +1,7 @@
 ﻿namespace HashBus.WebApi
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using LiteGuard;
 
     class IgnoredUserNamesService : IIgnoredUserNamesService
